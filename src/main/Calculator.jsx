f05534f72dcb2e4451baaa5ddbec382155a08c7b
weslles <joseweslles.b2@gmail.com>
2022-12-12 22:13:44 -0300

    Criando Botões

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Button from "../components/Button";
+import './Calculator.css';
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+                <Button label="AC"></Button>
+                <Button label="/"></Button>
+                <Button label="7"></Button>
+                <Button label="8"></Button>
+                <Button label="9"></Button>
+                <Button label="*"></Button>
+                <Button label="4"></Button>
+                <Button label="5"></Button>
+                <Button label="6"></Button>
+                <Button label="-"></Button>
+                <Button label="1"></Button>
+                <Button label="2"></Button>
+                <Button label="3"></Button>
+                <Button label="+"></Button>
+                <Button label="0"></Button>
+                <Button label="."></Button>
+                <Button label="="></Button>
+                
+            </div>
+        )
+    }
+}
